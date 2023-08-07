82631f493949d0095b283ddd9cf3ebd356a14d7a
Auric Vente <mprops@protonmail.com>
2023-08-07 17:01:42 -0600

    Seeeded random

--- utilz.js
+++ utilz.js
@@ -1036,6 +1036,39 @@ Utilz.is_at_bottom = (container) => {
   return container.scrollTop + container.clientHeight === container.scrollHeight
 }
 
+// Get seed for the other function
+Utils.get_seed = (str) => {
+  let h1 = 1779033703, h2 = 3144134277,
+  h3 = 1013904242, h4 = 2773480762
+
+  for (let i=0, k; i<str.length; i++) {
+    k = str.charCodeAt(i)
+    h1 = h2 ^ Math.imul(h1 ^ k, 597399067)
+    h2 = h3 ^ Math.imul(h2 ^ k, 2869860233)
+    h3 = h4 ^ Math.imul(h3 ^ k, 951274213)
+    h4 = h1 ^ Math.imul(h4 ^ k, 2716044179)
+  }
+
+  h1 = Math.imul(h3 ^ (h1 >>> 18), 597399067)
+  h2 = Math.imul(h4 ^ (h2 >>> 22), 2869860233)
+  h3 = Math.imul(h1 ^ (h3 >>> 17), 951274213)
+  h4 = Math.imul(h2 ^ (h4 >>> 19), 2716044179)
+  h1 ^= (h2 ^ h3 ^ h4), h2 ^= h1, h3 ^= h1, h4 ^= h1
+  return [h1 >>> 0, h2 >>> 0, h3 >>> 0, h4 >>> 0]
+}
+
+// Seeded random number generator based on a string
+Utils.seeded_random = (str) => {
+  let num = Utils.get_seed(str)[0]
+
+  return function() {
+    let t = num += 0x6D2B79F5
+    t = Math.imul(t ^ t >>> 15, t | 1)
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61)
+    return ((t ^ t >>> 14) >>> 0) / 4294967296
+  }
+}
+
 Utilz.media_types = [`image`, `tv`]
 Utilz.video_extensions = [`mp4`, `webm`]
 Utilz.video_types = [`video/mp4`, `video/webm`]
